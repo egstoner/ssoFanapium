581103dd103dafe2d8f8aa50645d46d2047f6eae
Emad Ghorbani Nia <e.ghorbaninia@fanap.ir>
2017-09-14 10:04:54 +0430

    First Commite
    1.Create class ssoFanapium

---
+++ sso/ssoFanapium.swift
@@ -0,0 +1,13 @@
+//
+//  ssoFanap.swift
+//  sso
+//
+//  Created by Emad Ghorbani Nia on 6/23/1396 AP.
+//  Copyright © 1396 AP Fanap. All rights reserved.
+//
+
+import UIKit
+
+class ssoFanapium: NSObject {
+
+}
